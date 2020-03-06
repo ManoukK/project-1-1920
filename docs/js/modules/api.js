9d63a5c6dd82a6e136ded5ac39cb76a89051fa67
ManoukK <manouk.kappe21@gmail.com>
2020-03-06 12:12:45 +0100

    laatste styling

--- docs/js/modules/api.js
+++ docs/js/modules/api.js
@@ -17,15 +17,20 @@ export async function getData(query){
     // detaillevel = default: Mirrors site output
     const detail = 'Default';
     const onlyBooks = 'type(book)';
-    //https://zoeken.oba.nl/api/v1/search/?q=paarden&authorization=dc0e2f073c03758140452044906bc818&detaillevel=Default&p=jeugd&output=json&refine=true&facet=type(book)
+  //https://zoeken.oba.nl/api/v1/search/?q=paarden&authorization=dc0e2f073c03758140452044906bc818&detaillevel=Default&p=jeugd&output=json&refine=true&facet=type(book)
 
     //deze url aanzetten als ik ga deployen
     const url = `${cors}${endpoint}${query}&authorization=${publicKey}&detaillevel=${detail}&p=${audience}&output=json&refine=true&facet=${onlyBooks}`;
-    //const url = `${endpoint}${query}&authorization=${publicKey}&detaillevel=${detail}&p=${audience}&output=json&refine=true&facet=${onlyBooks}`;
+    // const url = `${endpoint}${query}&authorization=${publicKey}&detaillevel=${detail}&p=${audience}&output=json&refine=true&facet=${onlyBooks}`;
+
+    // const url = ('data.json');
+    console.log(url)
+
 
 
     const data = await fetch(url)
         .then(function(response) {
+            console.log(response)
             return response.json();
         })
         .then(function(results) {
@@ -39,3 +44,5 @@ export async function getData(query){
         return data;
 };
 
+fetch('data.JSON')
+    .then(res => console.log('..',res))
